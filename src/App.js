2fca38f3bd9ffb8ee86cc13727846976b60ff49b
Dan Farrow <dan.farrow@adtrak.co.uk>
2020-08-25 17:29:16 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-xl text-yellow-500">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
